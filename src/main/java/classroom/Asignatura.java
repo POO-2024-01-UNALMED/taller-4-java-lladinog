0d00f18381a28e7f357da4158479353ec5baac98
lladinog <159490210+lladinog@users.noreply.github.com>
2024-03-05 11:06:29 -0500

    last

--- src/main/java/classroom/Asignatura.java
+++ src/main/java/classroom/Asignatura.java
@@ -3,57 +3,63 @@ package classroom;
 public class Asignatura {
 
     public String nombre;
-    public Integer codigoInterno;
-    public Integer codigoExterno;
+    String n = nombre;
+    public int codigoInterno;
+    public int codigoExterno;
     public Tipo tipo;
 
     public Asignatura() {
-        this.nombre = "Sin nombre";
+    	this.nombre = "Sin nombre";
         this.codigoInterno = 0;
         this.codigoExterno = 0;
         this.tipo = Tipo.FUNDAMENTACION;
     }
 
-    public Asignatura(int codigoInterno, Tipo tipo) {
-        this.nombre = "Sin nombre";
-        this.codigoInterno = codigoInterno;
-        this.codigoExterno = 0;
-        this.tipo = tipo;
-    }
-
-    public Asignatura( String nombre,int codigoExterno, Tipo tipo) {
-        this.nombre = nombre;
-        this.codigoInterno = 0;
+    public Asignatura(int codigoExterno,Tipo tipo) {
+    	this.nombre = "Sin nombre";
+        this.codigoInterno = codigoExterno;
         this.codigoExterno = codigoExterno;
         this.tipo = tipo;
     }
 
     public Asignatura(String nombre, Tipo tipo) {
-        this.nombre = nombre;
+    	this.nombre = nombre;
         this.codigoInterno = 0;
         this.codigoExterno = 0;
         this.tipo = tipo;
     }
 
-    public Asignatura(String nombre, Integer codigoInterno, Integer codigoExterno, Tipo tipo) {
+    public Asignatura(String nombre, int codigoInterno, int codigoExterno,Tipo tipo) {
         this.nombre = nombre;
         this.codigoInterno = codigoInterno;
         this.codigoExterno = codigoExterno;
         this.tipo = tipo;
     }
 
-    public void cambiarDatos(int nuevoCodigo) {
-        if (codigoInterno != null)
-            this.codigoInterno = nuevoCodigo;
-        else
-            this.codigoExterno = nuevoCodigo;
-    }
-
-    public void cambiarDatos(String nuevoNombre) {
-        this.nombre = nuevoNombre;
-    }
-
-    public void setCodigoInterno(Integer codigoInterno) {
+    public void cambiarDatos(int codigoInterno, int codigoExterno, String nombre,Tipo tipo) {
         this.codigoInterno = codigoInterno;
+        this.codigoExterno = codigoExterno;
+        this.nombre = nombre;
+        this.tipo = tipo;
+    }
+
+    public void cambiarDatos(int codigoInterno) {
+        this.codigoInterno = codigoInterno;
+    }
+
+    public void cambiarDatos(int codigoExterno) {
+        this.codigoExterno = codigoExterno;
+    }
+
+    public void cambiarDatos(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setCodigoInterno(int codigoInterno) {
+        this.codigoInterno = codigoInterno;
+    }
+    
+    public void setCodigoInterno(double codigoInterno) {
+        this.codigoInterno = (int) codigoInterno;
     }
 }
--- src/main/java/classroom/Persona.java
+++ src/main/java/classroom/Persona.java
@@ -1,37 +1,41 @@
 package classroom;
+
 public class Persona {
 
-	public long cedula;
-	private String nombre;
-    public static int totalPersonas;
+    final long cedula;
+    String nombre;
+    static int totalPersonas;
     
     static {
         totalPersonas = 0;
     }
     
     public Persona() {
-        this.cedula = 0; 
-        this.nombre = "";
+        this.cedula = 0;
+        this.nombre = null;
         totalPersonas++;
     }
+    
     public Persona(long cedula, String nombre) {
-        this.cedula = cedula;
-        this.nombre = nombre;
-        if (!nombre.isEmpty()) {
-            totalPersonas++;
-        }
+        this(nombre, cedula);
     }
     
     public Persona(String nombre, long cedula) {
-        this(cedula, nombre); 
+        this.cedula = cedula;
+        this.nombre = nombre;
+        totalPersonas++;
     }
 
     public Persona(long cedula) {
-        this(cedula, "");
+        this.cedula = cedula;
+        this.nombre = "";
+        totalPersonas++;
     }
 
     public Persona(String nombre) {
-        this(0, nombre);
+        this.nombre = nombre;
+        this.cedula = 1;
+        totalPersonas++;
     }
     
     public long getCedula() {

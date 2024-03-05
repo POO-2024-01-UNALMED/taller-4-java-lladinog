90676736c6799a7ea77c136a92b35e8a47f65896
lladinog <159490210+lladinog@users.noreply.github.com>
2024-03-05 03:59:05 -0500

    1

--- src/main/java/classroom/Persona.java
+++ src/main/java/classroom/Persona.java
@@ -1,34 +1,48 @@
 package classroom;
 
 public class Persona {
-    private String nombre;
-    private int cedula;
 
-    public Persona() {
+    final long cedula;
+    String nombre;
+    static int totalPersonas;
+
+    static {
+        totalPersonas = 0;
+    }
+
+    public Persona(long cedula, String nombre) {
+        this.cedula = cedula;
+        this.nombre = nombre;
+        totalPersonas++;
+    }
+
+    public Persona(String nombre, long cedula) {
+        this.cedula = cedula;
+        this.nombre = nombre;
+        totalPersonas++;
+    }
+
+    public Persona(long cedula) {
+        this.cedula = cedula;
         this.nombre = "";
-        this.cedula = 0;
+        totalPersonas++;
     }
 
     public Persona(String nombre) {
         this.nombre = nombre;
-        this.cedula = 1;
+        this.cedula = -1; // Asignamos un valor no válido a la cédula para indicar que no está definida
+        totalPersonas++;
     }
 
-    public Persona(int cedula) {
-        this.nombre = "";
-        this.cedula = cedula;
-    }
-
-    public Persona(String nombre, int cedula) {
-        this.nombre = nombre;
-        this.cedula = cedula;
-    }
-
-    public int getCedula() {
-        return this.cedula;
+    public long getCedula() {
+        return cedula;
     }
 
     public String getNombre() {
-        return this.nombre;
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
     }
 }
